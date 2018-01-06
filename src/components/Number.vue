64d70c2a8603116e814d36a74400c466d72fba3e
Andres Sarmiento <sarmiento@amatea.org>
2018-01-06 15:09:10 -0500

    install number

---
+++ src/components/Number.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="box">
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+  .box {
+    height: 300px;
+    width: 100%;
+    color: red;
+  }
+</style>
